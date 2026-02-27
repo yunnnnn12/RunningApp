9dab6451faf8cdd0dfca4cf2b122113b0280812a
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-02-27 15:40:31 +0900

    feat(repository): add SessionRepository for save session

---
+++ back/src/main/java/com/example/back/data/repository/SessionRepository.java
@@ -0,0 +1,7 @@
+package com.example.back.data.repository;
+
+import com.example.back.data.entity.RunSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<RunSession, Long> {
+}
