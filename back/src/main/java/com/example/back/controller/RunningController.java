693d8aedd1348f18458e89e96b9dbc5ea01cffa7
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-02-27 16:16:30 +0900

    chore(controller): add base RunningController template

---
+++ back/src/main/java/com/example/back/controller/RunningController.java
@@ -0,0 +1,11 @@
+package com.example.back.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/running")
+@RequiredArgsConstructor
+public class RunningController {
+}
