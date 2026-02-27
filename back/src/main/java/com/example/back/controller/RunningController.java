94023a12ba29e88ce495863493d765ad70d21d15
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-02-27 16:51:08 +0900

    feat(controller): startLocation and create first session API

--- back/src/main/java/com/example/back/controller/RunningController.java
+++ back/src/main/java/com/example/back/controller/RunningController.java
@@ -1,6 +1,11 @@
 package com.example.back.controller;
 
+import com.example.back.data.dto.LocationRequestDto;
+import com.example.back.data.entity.RunSession;
+import com.example.back.service.impl.RunningServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/running")
 @RequiredArgsConstructor
 public class RunningController {
+    private final RunningServiceImpl runningService;
+
+    @PostMapping(path = "/startLocation")
+    public RunSession getStartLocation(@RequestBody LocationRequestDto start){
+        return runningService.makeSession(start);
+    }
 }
