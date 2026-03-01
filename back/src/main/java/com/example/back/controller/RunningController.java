46685bdb4d9031db4a01c474b08d5cbb2e26188d
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-03-01 17:38:22 +0900

    feat(controller): add location save API

--- back/src/main/java/com/example/back/controller/RunningController.java
+++ back/src/main/java/com/example/back/controller/RunningController.java
@@ -1,6 +1,11 @@
 package com.example.back.controller;
 
+import com.example.back.data.dto.LocationRequestDto;
+import com.example.back.service.RunningService;
+import com.example.back.service.impl.RunningServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/running")
 @RequiredArgsConstructor
 public class RunningController {
+    private final RunningServiceImpl runningService;
+
+    @PostMapping("/location")
+    public void saveLocation(@RequestBody LocationRequestDto dto){
+        runningService.saveLocation(dto);
+    }
+
+
 }
