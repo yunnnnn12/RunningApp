642a44f2fda3cb142eb857919c63aaa9a75e8560
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-03-05 18:26:02 +0900

    feat(service): add distance calculation and endSession logic

--- back/src/main/java/com/example/back/service/impl/RunningServiceImpl.java
+++ back/src/main/java/com/example/back/service/impl/RunningServiceImpl.java
@@ -111,4 +111,39 @@ public class RunningServiceImpl implements RunningService {
 
         return sessionRepository.save(session); // 새로운 세션 기록
     }
+
+    private double caculateDistance(double lat1, double lon1,
+                                    double lat2, double lon2){
+        double R = 6371000; // meter
+
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2))
+                * Math.sin(dLon/2) * Math.sin(dLon/2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return R * c;
+    }
+
+    public RunSession endSession(Long sessionId) {
+
+        RunSession session = sessionRepository.findById(sessionId)
+                .orElseThrow(() -> new RuntimeException("Session not found"));
+
+        session.setEndTime(Instant.now()); // 완주 시간 기록
+
+        long totalTime = session.getEndTime().getEpochSecond() - session.getStartTime().getEpochSecond();
+
+        if (session.getTotalDistance() > 0) { // 평균 페이스 세팅
+            double km = session.getTotalDistance() / 1000.0;
+            double finalPace = totalTime / km;
+            session.setAveragePace(finalPace);
+        }
+
+        return sessionRepository.save(session); // 새로운 세션 기록
+    }
 }
