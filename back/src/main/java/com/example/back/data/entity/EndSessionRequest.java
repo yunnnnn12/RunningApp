668455c0695634e99594566723610c6f75df69cf
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-03-12 19:58:29 +0900

    refactor(entity): use DTO for end session request

---
+++ back/src/main/java/com/example/back/data/entity/EndSessionRequest.java
@@ -0,0 +1,14 @@
+package com.example.back.data.entity;
+
+public class EndSessionRequest {
+
+    private Long sessionId;
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+    }
+}
